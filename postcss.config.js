6e8717604ac9232010e04b3f5f699f238c105f57
hguencavdi-crypto <hguencavdi@gmail.com>
2025-11-17 07:30:32 +0100

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+4. "Commit changes"
+
+---
+
+### **📄 Dosya 5: `.gitignore`**
+
+1. "Add file" → "Create new file"
+2. Dosya adı: `.gitignore`
+3. İçerik:
+```
+node_modules
+.next
+.env.local
+.DS_Store
+*.log
